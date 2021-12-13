cc8ab853a4a72d7f864d982c42ad43309ddbba52
AntonioFuziy <antoniovf@al.insper.edu.br>
2021-12-13 00:52:20 -0300

    feat: added functions for mediapipe and pyautogui integration

--- tinder_controller.py
+++ tinder_controller.py
@@ -1,34 +1,44 @@
 import pyautogui
 
-
 def like():
     pyautogui.keyUp('right')
+    print("like")
 
 def dislike():
     pyautogui.keyUp('left')
+    print("dislike")
 
 def superlike():
     pyautogui.keyUp('enter')
+    print("superlike")
 
-def nextImage():
+def next_image():
     pyautogui.keyUp('space')
+    print("next image")
 
-def openProfile():
+def open_profile():
     pyautogui.keyUp('up')
+    print("open profile")
 
-def closeProfile():
+def close_profile():
     pyautogui.keyUp('down')
-def scrolUp(profileStatus):
+    print("close profile")
+def scroll_up(profileStatus):
     if profileStatus == 0:
         openProfile()
         profileStatus = 1
     pyautogui.scroll(100)
+    print("scroll up")
     return profileStatus
 
-def scrolDown(profileStatus):
+def scroll_down(profileStatus):
     if profileStatus == 0:
         openProfile()
         profileStatus = 1
     pyautogui.scroll(-100)
+    print("scroll down")
     return profileStatus
-nextImage()
+
+def close_window():
+    print("close window")
+    pyautogui.keyUp('esc')
